12850d21c497d3b4eb855cd939a019c3ff94b0d7
hexiaofeii <89957791+hexiaofeii@users.noreply.github.com>
2022-11-17 14:43:36 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Loving Page'),
+      home: const MyHomePage(title: 'Loving Page '),
     );
   }
 }
